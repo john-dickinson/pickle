0ac928ac8634fa45c01a14d262d0eb5ec8bdfcd5
John Dickinson <es@empathys-MacBook-Pro.local>
2019-11-10 12:25:47 -0500

    Hard commit to top nav

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-    <div>
-      <h1>Pickle Ras</h1>
-
+      <div class="top-nav" v-if="enterComplete">
+        <div class="flex">
+          <h1>Pickle Ras</h1>
+          <div class="side-nav">
+            <ul>
+              <li><router-link to="/">Home</router-link></li>
+              <li><router-link to="/illustrations">Illustrations</router-link></li>
+              <li><router-link to="/design">Design</router-link></li>
+              <li><router-link to="/prints">Prints</router-link></li>
+              <li><router-link to="/about">About</router-link></li>
+              <li><router-link to="/contact">Contact</router-link></li>
+            </ul>
+          </div>
+        </div>
+      <!-- <h1>Pickle Ras</h1> -->
+      </div>
       <!-- Entrance -->
       <Presite
         v-if="!enterComplete"
@@ -11,7 +24,7 @@
         :entered="siteEntered"
       />
 
-      <div class="flex" v-if="enterComplete">
+      <!-- <div class="flex" v-if="enterComplete">
         <div class="side-nav">
           <ul>
             <li><router-link to="/">Home</router-link></li>
@@ -22,12 +35,11 @@
             <li><router-link to="/contact">Contact</router-link></li>
           </ul>
         </div>
+      </div> 
+      </div>-->
 
-        <div class="main-body">
-          <router-view />
-        </div>
-      </div>
-
+    <div v-if="enterComplete" class="main-body">
+      <router-view />
     </div>
   </div>
 </template>
@@ -41,6 +53,7 @@ export default {
     return {
       siteEntered: false,
       enterComplete: false,
+      topNav: true,
     };
   },
   methods: {
@@ -58,7 +71,12 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  padding: 50px;
+  /* padding: 50px; */
+}
+
+body {
+  margin: 0;
+  padding: 0;
 }
 
 h1, h2 {
@@ -67,12 +85,13 @@ h1, h2 {
 
 ul {
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   padding: 0;
 }
 
 li {
   list-style: none;
+  margin: 0 8px;
 }
 
 a {
@@ -83,13 +102,30 @@ a {
   display: flex;
 }
 
+.top-nav .flex {
+  align-items: center;
+}
+
+.top-nav {
+  padding: 10px 50px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background: white;
+  z-index: 999;
+  border-bottom: 1px solid #ccc;
+}
+
 .side-nav {
-  margin-right: 150px;
+  margin-right: 50px;
+  margin-left: auto;
 }
 
 .main-body {
   display: flex;
   width: 100%;
+  padding: 50px;
+  margin-top: 75px;
   /* margin: 0 auto; */
 }
 .main-body > div {
