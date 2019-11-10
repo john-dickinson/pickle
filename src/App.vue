b6b3909d4db336b6f0313f78b97ce8a8930c5a19
John Dickinson <es@empathys-MacBook-Pro.local>
2019-11-10 13:30:47 -0500

    About page

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,36 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
-      <div class="top-nav" v-if="enterComplete">
-        <div class="flex">
-          <h1>Pickle Ras</h1>
-          <div class="side-nav">
-            <ul>
-              <li><router-link to="/">Home</router-link></li>
-              <li><router-link to="/illustrations">Illustrations</router-link></li>
-              <li><router-link to="/design">Design</router-link></li>
-              <li><router-link to="/prints">Prints</router-link></li>
-              <li><router-link to="/about">About</router-link></li>
-              <li><router-link to="/contact">Contact</router-link></li>
-            </ul>
-          </div>
-        </div>
-      <!-- <h1>Pickle Ras</h1> -->
-      </div>
-      <!-- Entrance -->
-      <Presite
-        v-if="!enterComplete"
-        :enter="enterSite"
-        :entered="siteEntered"
-      />
-
-      <!-- <div class="flex" v-if="enterComplete">
+    <div class="top-nav" v-if="enterComplete">
+      <div class="flex">
+        <h1>Nicole Ras</h1>
         <div class="side-nav">
           <ul>
-            <li><router-link to="/">Home</router-link></li>
-            <li><router-link to="/illustrations">Illustrations</router-link></li>
-            <li><router-link to="/design">Design</router-link></li>
-            <li><router-link to="/prints">Prints</router-link></li>
-            <li><router-link to="/about">About</router-link></li>
-            <li><router-link to="/contact">Contact</router-link></li>
+            <li>
+              <router-link to="/">Home</router-link>
+            </li>
+            <li>
+              <router-link to="/illustrations">Illustrations</router-link>
+            </li>
+            <li>
+              <router-link to="/design">Design</router-link>
+            </li>
+            <li>
+              <router-link to="/prints">Prints</router-link>
+            </li>
+            <li>
+              <router-link to="/about">About</router-link>
+            </li>
+            <li>
+              <router-link to="/contact">Contact</router-link>
+            </li>
           </ul>
         </div>
-      </div> 
-      </div>-->
+      </div>
+    </div>
+
+    <!-- Entrance -->
+    <Presite v-if="!enterComplete" :enter="enterSite" :entered="siteEntered" />
 
     <div v-if="enterComplete" class="main-body">
       <router-view />
@@ -45,29 +39,31 @@
 </template>
 
 <script>
-import Presite from '@/components/Presite';
+import Presite from "@/components/Presite";
 export default {
-  name: 'App',
+  name: "App",
   components: { Presite },
   data() {
     return {
       siteEntered: false,
       enterComplete: false,
-      topNav: true,
+      topNav: true
     };
   },
   methods: {
     enterSite() {
       this.siteEntered = true;
-      setTimeout(() => { this.enterComplete = true; }, 400);
-    },
-  },
+      setTimeout(() => {
+        this.enterComplete = true;
+      }, 400);
+    }
+  }
 };
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
@@ -79,7 +75,8 @@ body {
   padding: 0;
 }
 
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 
@@ -95,8 +92,16 @@ li {
 }
 
 a {
-  color: #42b983;
+  color: red;
+  text-decoration: none;
+  margin: 0;
+  font-size: 20px;
+  /* transition: all 300ms ease; */
 }
+
+/* a:hover {
+  transform: scale(0.9);
+} */
 
 .flex {
   display: flex;
@@ -114,21 +119,23 @@ a {
   background: white;
   z-index: 999;
   border-bottom: 1px solid #ccc;
+  /* box-shadow: 0px 2px 5px rgba(0, 0, 0, .20); */
 }
 
 .side-nav {
-  margin-right: 50px;
+  margin-right: 100px;
   margin-left: auto;
 }
 
 .main-body {
   display: flex;
   width: 100%;
-  padding: 50px;
+  /* padding: 50px; */
   margin-top: 75px;
   /* margin: 0 auto; */
 }
 .main-body > div {
   width: 100%;
+  padding: 50px;
 }
 </style>
